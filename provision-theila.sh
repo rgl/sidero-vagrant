b7fe3493de569de4d8871e803f22fb59d063c844
Rui Lopes <rgl@ruilopes.com>
2021-07-23 19:35:25 +0100

    upgrade to theila 0.1.0-alpha.2

--- provision-theila.sh
+++ provision-theila.sh
@@ -2,7 +2,7 @@
 source /vagrant/lib.sh
 
 
-theila_tag="${1:-v0.1.0-alpha.1}"; shift || true
+theila_tag="${1:-v0.1.0-alpha.2}"; shift || true
 
 theila_image="ghcr.io/talos-systems/theila:$theila_tag"
 
@@ -10,17 +10,18 @@ theila_image="ghcr.io/talos-systems/theila:$theila_tag"
 # until the theila image is available, we built it ourselfs.
 docker build -t "$theila_image" - <<EOF
 FROM ubuntu:20.04
+RUN apt-get update && apt-get install -y ca-certificates && rm -rf /var/lib/apt/lists/*
 ADD --chmod=755 https://github.com/talos-systems/theila/releases/download/$theila_tag/theila-linux-amd64 /theila
 # TODO remove the next line after https://github.com/moby/buildkit/pull/2171 lands in a docker release.
 RUN chmod 755 /theila
-ENTRYPOINT ["/theila"]
+ENTRYPOINT ["/theila", "--address", "0.0.0.0"]
 EOF
 
 title 'Starting theila'
 docker run -d \
     --restart=unless-stopped \
     --name theila \
-    -p 8080:8080/tcp \
+    --net host \
     -v "$HOME/.talos:/root/.talos:ro" \
     -v "$HOME/.kube:/root/.kube:ro" \
     "$theila_image"
